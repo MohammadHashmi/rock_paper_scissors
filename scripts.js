f2339d6070212ead940c342aec55fadf0003b740
Mohammad Hashmi <mohammad.hashmi2005@gmail.com>
2022-01-30 02:49:30 -0500

    Got single game function working, haven't tried with previous computerPlay function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
 //Computer returns a random option
 function computerPlay() {
-    let computerChoices = ["Rock", "Paper", "Scissors"];
+    let computerChoices = ["rock", "paper", "scissors"];
     let computerDecision = Math.floor(Math.random() * computerChoices.length);
     let computerTurn = computerChoices[computerDecision];
     return computerTurn;
 }
 
-computerPlay(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase === 'Rock')
+//Plays a single round of rock, paper, scissors to determine a winner
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === "paper") {
+        return ("You lost :(")
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === "scissors") {
+        return ("You won :)))")
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === "scissors") {
+        return ("You lost :(")
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === "rock") {
+        return ("You won :)))")
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "rock") {
+        return ("You lost :(")
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "paper") {
+        return ("You won :)))")
+    } else if (playerSelection.toLowerCase() === computerSelection) {
+        return ("Tie Game!")
+    } 
 }
+
+let playerChoice = 'scissors'
+let hypocomputer = "paper"
+console.log(playRound(playerChoice, hypocomputer))
